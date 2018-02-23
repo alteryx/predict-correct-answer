3d86e4fdef654bd4e589fe07963f4354bed8b1c8
Seth-Rothschild <seth.rothschild@featurelabs.com>
2018-02-23 18:17:58 -0500

    Move more to utils

--- README.md
+++ README.md
@@ -14,13 +14,16 @@ In this tutorial, we show how to use [Featuretools](https://www.featuretools.com
 
 ## Demonstration
 
-The main notebook can be found [here](Demo%20-%20LearnLab.ipynb). The notebook relies on a `learnlab_to_entityset` function which is described in depth in the [entityset_function notebook](entityset_function.ipynb). 
+The main notebook can be found [here](Demo%20-%20LearnLab.ipynb). 
 
-To run either notebook, you will need to download Featuretools with
+To run the notebook, you will need to download Featuretools with
 ```
 pip install featuretools
 ```
-and the geometry dataset from [the datashop website](https://pslcdatashop.web.cmu.edu/DatasetInfo?datasetId=76) (free account required). Take the `.txt` file from the zipped download and place it in the `data` folder in this repository.
+and the geometry dataset from [the datashop website](https://pslcdatashop.web.cmu.edu/DatasetInfo?datasetId=76) (free account required). Take the `.txt` file from the zipped download and place it in the `data` folder in this repository. The notebook relies on a `learnlab_to_entityset` function which is described in depth in the [entityset_function notebook](entityset_function.ipynb). 
 
 ## Feature Labs
+<a href="https://www.featurelabs.com/">
+    <img src="http://www.featurelabs.com/wp-content/uploads/2017/12/logo.png" alt="Featuretools" />
+</a>
 Featuretools was created by the developers at [Feature Labs](https://www.featurelabs.com/). If building impactful data science pipelines is important to you or your business, please [get in touch](https://www.featurelabs.com/contact.html).
--- utils.py
+++ utils.py
@@ -1,6 +1,12 @@
 import featuretools.variable_types as vtypes
 import pandas as pd
 import featuretools as ft
+from featuretools.primitives import Sum, Mean, Median, Count, Hour
+from featuretools.selection import remove_low_information_features
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import roc_auc_score
+
+
 def learnlab_to_entityset(data):
     # Make an EntitySet called Dataset with the following structure
     #
@@ -19,15 +25,14 @@ def learnlab_to_entityset(data):
     data['Outcome'] = data['Outcome'].map({'INCORRECT': 0, 'CORRECT': 1})
     data['End Time'] = pd.to_datetime(data['Time']) + pd.to_timedelta(pd.to_numeric(data['Duration (sec)']), 's')
 
-
     es = ft.EntitySet('Dataset')
-    es.entity_from_dataframe(entity_id='transactions', 
-                             index='Transaction Id', 
+    es.entity_from_dataframe(entity_id='transactions',
+                             index='Transaction Id',
                              dataframe=data,
                              variable_types={'Outcome': vtypes.Boolean},
                              time_index='Time',
                              secondary_time_index={'End Time': ['Outcome', 'Is Last Attempt', 'Duration (sec)']})
-    
+
     # Two entities associated to problems
     es.normalize_entity(base_entity_id='transactions',
                         new_entity_id='problem_steps',
@@ -39,8 +44,7 @@ def learnlab_to_entityset(data):
                         new_entity_id='problems',
                         index='Problem Name',
                         make_time_index=False)
-    
-    
+
     # Two entities associated to students
     es.normalize_entity(base_entity_id='transactions',
                         new_entity_id='sessions',
@@ -52,14 +56,14 @@ def learnlab_to_entityset(data):
                         new_entity_id='students',
                         index='Anon Student Id',
                         make_time_index=True)
-    
+
     # Two entities associated to a school
     es.normalize_entity(base_entity_id='transactions',
                         new_entity_id='classes',
                         index='Class',
                         additional_variables=['School'],
                         make_time_index=False)
-    
+
     es.normalize_entity(base_entity_id='classes',
                         new_entity_id='schools',
                         index='School',
@@ -72,3 +76,40 @@ def learnlab_to_entityset(data):
                         additional_variables=[],
                         make_time_index=False)
     return es
+
+
+def autorun_dfs(es, target_entity='transactions',
+                label='Outcome', custom_agg=[]):
+    cutoff_times = es[target_entity].df[['Transaction Id', 'End Time', label]]
+    fm, features = ft.dfs(entityset=es,
+                          target_entity='transactions',
+                          agg_primitives=[Sum, Mean] + custom_agg,
+                          trans_primitives=[Hour],
+                          max_depth=3,
+                          approximate='2m',
+                          cutoff_time=cutoff_times,
+                          verbose=True)
+    fm_enc, _ = ft.encode_features(fm, features)
+    fm_enc = fm_enc.fillna(0)
+    fm_enc = remove_low_information_features(fm_enc)
+    labels = fm.pop(label)
+    return (fm_enc, labels)
+
+
+def score_with_tssplit(fm_enc, label, splitter):
+    k = 0
+    for train_index, test_index in splitter.split(fm_enc):
+        clf = RandomForestClassifier()
+        X_train, X_test = fm_enc.iloc[train_index], fm_enc.iloc[test_index]
+        y_train, y_test = label[train_index], label[test_index]
+        clf.fit(X_train, y_train)
+        preds = clf.predict(X_test)
+        score = round(roc_auc_score(preds, y_test), 2)
+        print("AUC score on time split {} is {}".format(k, score))
+        feature_imps = [(imp, fm_enc.columns[i]) for i, imp in enumerate(clf.feature_importances_)]
+        feature_imps.sort()
+        feature_imps.reverse()
+        print("Top 5 features: {}".format([f[1] for f in feature_imps[0:5]]))
+        print("-----\n")
+
+        k += 1
